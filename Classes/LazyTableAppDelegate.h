71e83d095679b91e327ede0be0177c74d9f02cdf
Codemis <johnathan@missionaldigerati.com>
2012-11-08 20:05:17 -0800

    Removed bogus headers.

--- Classes/LazyTableAppDelegate.h
+++ Classes/LazyTableAppDelegate.h
@@ -1,5 +1,3 @@
-#import "AppRecord.h"
-#import "ParseOperation.h"
 @interface LazyTableAppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @property (nonatomic, strong) NSMutableArray *appRecords;
