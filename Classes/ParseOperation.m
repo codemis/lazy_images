2d63ceeaa0d74591861f5399556983bbc43c87e5
Codemis <johnathan@missionaldigerati.com>
2012-11-08 20:27:46 -0800

    Cleaned up Parsing

--- Classes/ParseOperation.m
+++ Classes/ParseOperation.m
@@ -1,62 +1,11 @@
-/*
-     File: ParseOperation.m 
- Abstract: NSOperation code for parsing the RSS feed.
-  
-  Version: 1.3 
-  
- Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple 
- Inc. ("Apple") in consideration of your agreement to the following 
- terms, and your use, installation, modification or redistribution of 
- this Apple software constitutes acceptance of these terms.  If you do 
- not agree with these terms, please do not use, install, modify or 
- redistribute this Apple software. 
-  
- In consideration of your agreement to abide by the following terms, and 
- subject to these terms, Apple grants you a personal, non-exclusive 
- license, under Apple's copyrights in this original Apple software (the 
- "Apple Software"), to use, reproduce, modify and redistribute the Apple 
- Software, with or without modifications, in source and/or binary forms; 
- provided that if you redistribute the Apple Software in its entirety and 
- without modifications, you must retain this notice and the following 
- text and disclaimers in all such redistributions of the Apple Software. 
- Neither the name, trademarks, service marks or logos of Apple Inc. may 
- be used to endorse or promote products derived from the Apple Software 
- without specific prior written permission from Apple.  Except as 
- expressly stated in this notice, no other rights or licenses, express or 
- implied, are granted by Apple herein, including but not limited to any 
- patent rights that may be infringed by your derivative works or by other 
- works in which the Apple Software may be incorporated. 
-  
- The Apple Software is provided by Apple on an "AS IS" basis.  APPLE 
- MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
- THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS 
- FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND 
- OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS. 
-  
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL 
- OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, 
- MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED 
- AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), 
- STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE 
- POSSIBILITY OF SUCH DAMAGE. 
-  
- Copyright (C) 2012 Apple Inc. All Rights Reserved. 
-  
- */
-
 #import "ParseOperation.h"
 #import "AppRecord.h"
 #import "LazyTableAppDelegate.h"
-
-// string contants found in the RSS feed
 static NSString *kIDStr     = @"id";
 static NSString *kNameStr   = @"im:name";
 static NSString *kImageStr  = @"im:image";
 static NSString *kArtistStr = @"im:artist";
 static NSString *kEntryStr  = @"entry";
-
 
 @interface ParseOperation ()
 @property (nonatomic, copy) ArrayBlock completionHandler;
@@ -70,9 +19,9 @@ static NSString *kEntryStr  = @"entry";
 
 @implementation ParseOperation
 
-@synthesize completionHandler, errorHandler, dataToParse, workingArray, workingEntry, workingPropertyString, elementsToParse, storingCharacterData;
 
-- (id)initWithData:(NSData *)data completionHandler:(ArrayBlock)handler
+- (id)      initWithData:(NSData *)data
+       completionHandler:(ArrayBlock)handler
 {
     self = [super init];
     if (self != nil)
@@ -83,60 +32,40 @@ static NSString *kEntryStr  = @"entry";
     }
     return self;
 }
-
-// -------------------------------------------------------------------------------
-//	dealloc:
-// -------------------------------------------------------------------------------
-
-// -------------------------------------------------------------------------------
-//	main:
-//  Given data to parse, use NSXMLParser and process all the top paid apps.
-// -------------------------------------------------------------------------------
 - (void)main
 {
 	@autoreleasepool {
-	
-		self.workingArray = [NSMutableArray array];
-    self.workingPropertyString = [NSMutableString string];
-    
-    // It's also possible to have NSXMLParser download the data, by passing it a URL, but this is not
-		// desirable because it gives less control over the network, particularly in responding to
-		// connection errors.
-    //
-    NSXMLParser *parser = [[NSXMLParser alloc] initWithData:dataToParse];
-		[parser setDelegate:self];
-    [parser parse];
-		
-		if (![self isCancelled])
-    {
-        // call our completion handler with the result of our parsing
-        self.completionHandler(self.workingArray);
-    }
-    
-    self.workingArray = nil;
-    self.workingPropertyString = nil;
-    self.dataToParse = nil;
-    
-
+        self.workingArray = [NSMutableArray array];
+        self.workingPropertyString = [NSMutableString string];
+        
+        // It's also possible to have NSXMLParser download the data, by passing it a URL, but this is not
+        // desirable because it gives less control over the network, particularly in responding to
+        // connection errors.
+        //
+        NSXMLParser *parser = [[NSXMLParser alloc] initWithData:self.dataToParse];
+        [parser setDelegate:self];
+        [parser parse];
+            
+        if (![self isCancelled])
+        {
+            self.completionHandler(self.workingArray);
+        }    
+        self.workingArray = nil;
+        self.workingPropertyString = nil;
+        self.dataToParse = nil;
 	}
 }
-
-
-#pragma mark -
-#pragma mark RSS processing
-
+#pragma mark - RSS processing
 - (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName
                                         namespaceURI:(NSString *)namespaceURI
                                        qualifiedName:(NSString *)qName
                                           attributes:(NSDictionary *)attributeDict
 {
-    // entry: { id (link), im:name (app name), im:image (variable height) }
-    //
     if ([elementName isEqualToString:kEntryStr])
 	{
         self.workingEntry = [[AppRecord alloc] init];
     }
-    storingCharacterData = [elementsToParse containsObject:elementName];
+    self.storingCharacterData = [self.elementsToParse containsObject:elementName];
 }
 
 - (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName
@@ -145,11 +74,11 @@ static NSString *kEntryStr  = @"entry";
 {
     if (self.workingEntry)
 	{
-        if (storingCharacterData)
+        if (self.storingCharacterData)
         {
-            NSString *trimmedString = [workingPropertyString stringByTrimmingCharactersInSet:
+            NSString *trimmedString = [self.workingPropertyString stringByTrimmingCharactersInSet:
                                        [NSCharacterSet whitespaceAndNewlineCharacterSet]];
-            [workingPropertyString setString:@""];  // clear the string for next time
+            [self.workingPropertyString setString:@""];
             if ([elementName isEqualToString:kIDStr])
             {
                 self.workingEntry.appURLString = trimmedString;
@@ -173,20 +102,16 @@ static NSString *kEntryStr  = @"entry";
             self.workingEntry = nil;
         }
     }
-    
 }
-
 - (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
 {
-    if (storingCharacterData)
+    if (self.storingCharacterData)
     {
-        [workingPropertyString appendString:string];
+        [self.workingPropertyString appendString:string];
     }
 }
-
 - (void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError
 {
     self.errorHandler(parseError);
 }
-
 @end
