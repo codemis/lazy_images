a57a45d489b1b802c7191029032cf9e1b570f468
Codemis <johnathan@missionaldigerati.com>
2012-11-08 21:08:16 -0800

    Cleaned up Root Controller.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -1,119 +1,34 @@
-/*
-     File: RootViewController.m 
- Abstract: Controller for the main table view of the LazyTable sample.
-    This table view controller works off the AppDelege's data model.
-    produce a three-stage lazy load:
-    1. No data (i.e. an empty table)
-    2. Text-only data from the model's RSS feed
-    3. Images loaded over the network asynchronously
- 
-    This process allows for asynchronous loading of the table to keep the UI responsive.
-    Stage 3 is managed by the AppRecord corresponding to each row/cell.
- 
-    Images are scaled to the desired height.
-    If rapid scrolling is in progress, downloads do not begin until scrolling has ended.
-  
-  Version: 1.3 
-  
- Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple 
- Inc. ("Apple") in consideration of your agreement to the following 
- terms, and your use, installation, modification or redistribution of 
- this Apple software constitutes acceptance of these terms.  If you do 
- not agree with these terms, please do not use, install, modify or 
- redistribute this Apple software. 
-  
- In consideration of your agreement to abide by the following terms, and 
- subject to these terms, Apple grants you a personal, non-exclusive 
- license, under Apple's copyrights in this original Apple software (the 
- "Apple Software"), to use, reproduce, modify and redistribute the Apple 
- Software, with or without modifications, in source and/or binary forms; 
- provided that if you redistribute the Apple Software in its entirety and 
- without modifications, you must retain this notice and the following 
- text and disclaimers in all such redistributions of the Apple Software. 
- Neither the name, trademarks, service marks or logos of Apple Inc. may 
- be used to endorse or promote products derived from the Apple Software 
- without specific prior written permission from Apple.  Except as 
- expressly stated in this notice, no other rights or licenses, express or 
- implied, are granted by Apple herein, including but not limited to any 
- patent rights that may be infringed by your derivative works or by other 
- works in which the Apple Software may be incorporated. 
-  
- The Apple Software is provided by Apple on an "AS IS" basis.  APPLE 
- MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
- THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS 
- FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND 
- OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS. 
-  
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL 
- OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, 
- MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED 
- AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), 
- STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE 
- POSSIBILITY OF SUCH DAMAGE. 
-  
- Copyright (C) 2012 Apple Inc. All Rights Reserved. 
-  
- */
-
 #import "RootViewController.h"
 #import "AppRecord.h"
-
-#define kCustomRowHeight    60.0
+#import "IconDownloader.h"
 #define kCustomRowCount     7
-
 #pragma mark -
-
-@interface RootViewController ()
-
+@interface RootViewController ()<UIScrollViewDelegate, IconDownloaderDelegate>
 - (void)startIconDownload:(AppRecord *)appRecord forIndexPath:(NSIndexPath *)indexPath;
-
 @end
 
 @implementation RootViewController
-
-@synthesize entries;
-@synthesize imageDownloadsInProgress;
-
-
 #pragma mark 
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
     self.imageDownloadsInProgress = [NSMutableDictionary dictionary];
-    self.tableView.rowHeight = kCustomRowHeight;
 }
-
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    
     // terminate all pending download connections
     NSArray *allDownloads = [self.imageDownloadsInProgress allValues];
     [allDownloads makeObjectsPerformSelector:@selector(cancelDownload)];
 }
-
-#pragma mark -
-#pragma mark Table view creation (UITableViewDataSource)
-
-// customize the number of rows in the table view
+#pragma mark - Table view creation (UITableViewDataSource)
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-	int count = [entries count];
-	
-	// ff there's no data yet, return enough rows to fill the screen
-    if (count == 0)
-	{
-        return kCustomRowCount;
-    }
-    return count;
+	int count = [self.entries count];
+    return (count == 0) ? kCustomRowCount : count;
 }
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	static NSString *CellIdentifier = @"LazyTableCell";
     static NSString *PlaceholderCellIdentifier = @"PlaceholderCell";
@@ -124,16 +39,12 @@
     }
 	
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    // TODO: Why?!?
-    // Leave cells empty if there's no data yet
+    // Leave cells empty because 7 are empty on load
     if (nodeCount > 0)
 	{
-        // Set up the cell...
         AppRecord *appRecord = (self.entries)[indexPath.row];
-        
 		cell.textLabel.text = appRecord.appName;
         cell.detailTextLabel.text = appRecord.artist;
-		
         // Only load cached images; defer new downloads until scrolling ends
         if (!appRecord.appIcon)
         {
@@ -141,37 +52,30 @@
             {
                 [self startIconDownload:appRecord forIndexPath:indexPath];
             }
-            // if a download is deferred or in progress, return a placeholder image
             cell.imageView.image = [UIImage imageNamed:@"Placeholder.png"];                
         }
         else
         {
            cell.imageView.image = appRecord.appIcon;
         }
-
     }
-    
     return cell;
 }
-
-
-#pragma mark -
-#pragma mark Table cell image support
-
-- (void)startIconDownload:(AppRecord *)appRecord forIndexPath:(NSIndexPath *)indexPath
+#pragma mark - Table cell image support
+- (void)startIconDownload:(AppRecord *)appRecord
+             forIndexPath:(NSIndexPath *)indexPath
 {
-    IconDownloader *iconDownloader = imageDownloadsInProgress[indexPath];
+    IconDownloader *iconDownloader = self.imageDownloadsInProgress[indexPath];
     if (iconDownloader == nil) 
     {
         iconDownloader = [[IconDownloader alloc] init];
         iconDownloader.appRecord = appRecord;
         iconDownloader.indexPathInTableView = indexPath;
         iconDownloader.delegate = self;
-        imageDownloadsInProgress[indexPath] = iconDownloader;
+        self.imageDownloadsInProgress[indexPath] = iconDownloader;
         [iconDownloader startDownload];
     }
 }
-
 // this method is used in case the user scrolled into a set of cells that don't have their app icons yet
 - (void)loadImagesForOnscreenRows
 {
@@ -182,36 +86,25 @@
         {
             AppRecord *appRecord = (self.entries)[indexPath.row];
             
-            if (!appRecord.appIcon) // avoid the app icon download if the app already has an icon
+            if (!appRecord.appIcon)
             {
                 [self startIconDownload:appRecord forIndexPath:indexPath];
             }
         }
     }
 }
-
-// called by our ImageDownloader when an icon is ready to be displayed
 - (void)appImageDidLoad:(NSIndexPath *)indexPath
 {
-    IconDownloader *iconDownloader = imageDownloadsInProgress[indexPath];
+    IconDownloader *iconDownloader = self.imageDownloadsInProgress[indexPath];
     if (iconDownloader != nil)
     {
-        UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:iconDownloader.indexPathInTableView];
-        
-        // Display the newly loaded image
+        UITableViewCell *cell =
+          [self.tableView cellForRowAtIndexPath:iconDownloader.indexPathInTableView];
         cell.imageView.image = iconDownloader.appRecord.appIcon;
     }
-    
-    // Remove the IconDownloader from the in progress list.
-    // This will result in it being deallocated.
-    [imageDownloadsInProgress removeObjectForKey:indexPath];
+    [self.imageDownloadsInProgress removeObjectForKey:indexPath];
 }
-
-
-#pragma mark -
-#pragma mark Deferred image loading (UIScrollViewDelegate)
-
-// Load images for all onscreen rows when scrolling is finished
+#pragma mark - Deferred image loading (UIScrollViewDelegate)
 - (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
 {
     if (!decelerate)
@@ -219,7 +112,6 @@
         [self loadImagesForOnscreenRows];
     }
 }
-
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
 {
     [self loadImagesForOnscreenRows];
