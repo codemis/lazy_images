a0741f25ad5018d2e49e9a0359f59b0c50082d3b
Bruce Hobbs <brucehobbs@engineeredsw.com>
2012-11-28 21:22:08 -0800

    Removed unused AppRecord properties; added SDWebImage "library" and updated AppTableViewController appropriately

--- Classes/AppTableViewController.h
+++ Classes/AppTableViewController.h
@@ -1,4 +1,3 @@
 @interface AppTableViewController : UITableViewController
 @property (nonatomic, strong) NSArray *entries;
-@property (nonatomic, strong) NSMutableDictionary *imageDownloadsInProgress;
 @end
